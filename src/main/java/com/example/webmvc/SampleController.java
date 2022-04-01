1d26026cdd83073a1678f2a8b22b779829782f6a
dev-hjkim <dev--test@naver.com>
2022-04-01 21:24:25 +0900

    220401, head & options setting

--- src/main/java/com/example/webmvc/SampleController.java
+++ src/main/java/com/example/webmvc/SampleController.java
@@ -2,20 +2,22 @@ package com.example.webmvc;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping(
-            value = "/hello",
-//            headers = "!" + HttpHeaders.FROM
-            headers = HttpHeaders.AUTHORIZATION + "=" + "111",
-            params = "name=hjkim"
-    )
+    @GetMapping(value = "/hello")
     @ResponseBody
     public String hello() {
+        return "hello";
+    }
+
+    @PostMapping(value = "/hello")
+    @ResponseBody
+    public String helloPost() {
         return "hello";
     }
 }
--- src/test/java/com/example/webmvc/SampleControllerTest.java
+++ src/test/java/com/example/webmvc/SampleControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,13 +22,15 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello")
-                        .header(HttpHeaders.AUTHORIZATION, "111")
-//                        .accept(MediaType.APPLICATION_JSON)
-                                .param("name", "hjkim")
-                )
+        mockMvc.perform(options("/hello"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(header().exists(HttpHeaders.ALLOW))
+                .andExpect(header().stringValues(HttpHeaders.ALLOW,
+                        hasItems(containsString("GET"),
+                                containsString("POST"),
+                                containsString("OPTIONS"),
+                                containsString("HEAD"))))
         ;
     }
 }
