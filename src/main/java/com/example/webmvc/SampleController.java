5271f74754d8b98fd9ec6f370d88347618185c25
dev-hjkim <dev--test@naver.com>
2022-04-01 20:53:34 +0900

    220401, mediaType setting

--- src/main/java/com/example/webmvc/SampleController.java
+++ src/main/java/com/example/webmvc/SampleController.java
@@ -1,28 +1,20 @@
 package com.example.webmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
-//    @GetMapping({"/hello", "/hi"})
-//    @GetMapping("/hello?")    //hello 뒤에 한 글자(ex. /hello1, /hello2)
-//    @GetMapping("/hello/*")   //hello 뒤에 여러 글자(ex. /hello/123. hello/abc)
-//    @GetMapping("/hello/**")    //hello 뒤에 여러 path(ex. /hello/123/456)
-    @RequestMapping("/**")
+    @RequestMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
     @ResponseBody
     public String hello() {
         return "hello";
-    }
-
-    @RequestMapping("/{name:[a-z]+}")
-    @ResponseBody
-    public String helloWithName(@PathVariable String name) {
-        return "hello " + name;
     }
 }
--- src/test/java/com/example/webmvc/SampleControllerTest.java
+++ src/test/java/com/example/webmvc/SampleControllerTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,20 +20,12 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello/123/456"))
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                        .accept(MediaType.APPLICATION_JSON))
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
-        ;
-    }
-
-    @Test
-    public void helloWithNameTest() throws Exception {
-        mockMvc.perform(get("/hello/hjkim"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello hjkim")) // 가장 구체적인 uri의 handler가 사용된다!
-                .andExpect(handler().handlerType(SampleController.class))
-                .andExpect(handler().methodName("helloWithName"))
         ;
     }
 }
