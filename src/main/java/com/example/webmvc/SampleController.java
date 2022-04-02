7a0f02456a7545c21b0627f6b6bee41c2e3b2bad
dev-hjkim <dev--test@naver.com>
2022-04-03 03:26:52 +0900

    220403, request mapping practice

--- src/main/java/com/example/webmvc/SampleController.java
+++ src/main/java/com/example/webmvc/SampleController.java
@@ -1,14 +1,34 @@
 package com.example.webmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class SampleController {
 
     @GetHelloMapping
     @ResponseBody
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/events")
+    @ResponseBody
+    public String events() {
+        return "events";
+    }
+
+    @GetMapping("/events/{id}")
+    @ResponseBody
+    public String getAnEvent(@PathVariable int id) {
+        return "event";
+    }
+
+    @DeleteMapping("/events/{id}")
+    @ResponseBody
+    public String removeEvents(@PathVariable int id) {
+        return "event";
     }
 }
--- src/test/java/com/example/webmvc/SampleControllerTest.java
+++ src/test/java/com/example/webmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,4 +25,47 @@ public class SampleControllerTest {
                 .andExpect(status().isOk())
         ;
     }
+
+    @Test
+    public void getEvents() throws Exception {
+        mockMvc.perform(get("/events"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getEventsWithId() throws Exception {
+        mockMvc.perform(get("/events/1"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/events/2"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/events/3"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteEvent() throws Exception {
+        mockMvc.perform(delete("/events/1"))
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/events/2"))
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/events/3"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateEvent() throws Exception {
+        mockMvc.perform(put("/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
 }
