0559195afe09e8e3f6b1bbd9891166f344404723
dev-hjkim <dev--test@naver.com>
2022-04-02 22:40:15 +0900

    220402, custom annotation

--- src/main/java/com/example/webmvc/SampleController.java
+++ src/main/java/com/example/webmvc/SampleController.java
@@ -1,23 +1,14 @@
 package com.example.webmvc;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @GetMapping(value = "/hello")
+    @GetHelloMapping
     @ResponseBody
     public String hello() {
-        return "hello";
-    }
-
-    @PostMapping(value = "/hello")
-    @ResponseBody
-    public String helloPost() {
         return "hello";
     }
 }
--- src/test/java/com/example/webmvc/SampleControllerTest.java
+++ src/test/java/com/example/webmvc/SampleControllerTest.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,15 +19,9 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(options("/hello"))
+        mockMvc.perform(get("/hello"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(header().exists(HttpHeaders.ALLOW))
-                .andExpect(header().stringValues(HttpHeaders.ALLOW,
-                        hasItems(containsString("GET"),
-                                containsString("POST"),
-                                containsString("OPTIONS"),
-                                containsString("HEAD"))))
         ;
     }
 }
